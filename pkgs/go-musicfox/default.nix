34a67a884c20c2a0acbbcd70e5afb1808d68ac3c
Hertz Hwang <hertz@26hz.com.cn>
2023-04-21 20:55:19 +0800

    update[go-musicfox]: v4.0.3 -> v4.0.4

--- pkgs/go-musicfox/default.nix
+++ pkgs/go-musicfox/default.nix
@@ -10,7 +10,7 @@
 # gcc only supports objc on darwin
 buildGoModule.override { stdenv = clangStdenv; } rec {
   pname = "go-musicfox";
-  version = "4.0.3";
+  version = "4.0.4";
 
   src = fetchFromGitHub {
     owner = "anhoder";
