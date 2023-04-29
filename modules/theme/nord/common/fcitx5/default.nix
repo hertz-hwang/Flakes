a3c1b761917781d76946d7684c538815c08e83ea
Hertz Hwang <hertz@26hz.com.cn>
2023-04-30 00:00:51 +0800

    add[fcitx5]: My personal rime configurations of input method

--- modules/theme/nord/common/fcitx5/default.nix
+++ modules/theme/nord/common/fcitx5/default.nix
@@ -2,8 +2,11 @@
 {
   home.file.".config/fcitx5/conf/classicui.conf".source = ./classicui.conf;
   home.file.".local/share/fcitx5/themes/Nord/theme.conf".text = import ./theme.nix;
-  #home.file.".local/share/fcitx5/rime" = {
-  #  source = ./_user-data;
-  #  recursive = true;
-  #};
+  home.file.".local/share/fcitx5/rime" = {
+    source = builtins.fetchGit {
+      url = https://git.26hz.com.cn/hertz/rime;
+      rev = "891e5bb54245864936c3d95f7e0a9d4e51352ba4";
+    };
+    recursive = true;
+  };
 }
