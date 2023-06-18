21dcef16489f63ef0af69f423501c0041226d08b
Hertz Hwang <hertz@26hz.com.cn>
2023-06-18 09:12:21 +0800

    update[go-musicfox]: v4.0.6 -> v4.1.2

--- pkgs/go-musicfox/default.nix
+++ pkgs/go-musicfox/default.nix
@@ -10,13 +10,13 @@
 # gcc only supports objc on darwin
 buildGoModule.override { stdenv = clangStdenv; } rec {
   pname = "go-musicfox";
-  version = "4.0.6";
+  version = "4.1.2";
 
   src = fetchFromGitHub {
-    owner = "anhoder";
+    owner = "go-musicfox";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-ZqB3NL/pLIY1lHl3qMIOciqsOW9jNwjVQAq1j/ydDWs=";
+    hash = "sha256-ushJZKZPIUo6IO33E9A/CneLHHrE6MtdwpCYR4ivHmo=";
   };
 
   deleteVendor = true;
@@ -42,7 +42,7 @@ buildGoModule.override { stdenv = clangStdenv; } rec {
 
   meta = with lib; {
     description = "Terminal netease cloud music client written in Go";
-    homepage = "https://github.com/anhoder/go-musicfox";
+    homepage = "https://github.com/go-musicfox/go-musicfox";
     license = licenses.mit;
     mainProgram = "musicfox";
     maintainers = with maintainers; [ zendo Ruixi-rebirth Hertz ];
